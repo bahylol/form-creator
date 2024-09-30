fd7f48d3caf8c2b5db4b82eeeaec9de9d3ed71cb
bahylol <bahymohamed2010@gmail.com>
2024-10-01 00:42:31 +0300

    added a text example

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import FormCreator from './components/FormCreator';
 
 function App() {
 
-  const formTemplate = [
+  const formTemplate1 = [
     { label: 'First Name', name: 'firstName', type: 'text', placeholder: 'Enter your first name' },
     { label: 'Last Name', name: 'lastName', type: 'text', placeholder: 'Enter your last name' },
     { label: 'Email', name: 'email', type: 'email', placeholder: 'Enter your email' },
     { label: 'Phone', name: 'phone', type: 'tel', placeholder: 'Enter your phone number' },
+  ];
+
+  const formTemplate2 = [
+    { label: 'First Name', name: 'firstName', type: 'text', placeholder: 'Enter your first name' },
+    { label: 'Last Name', name: 'lastName', type: 'text', placeholder: 'Enter your last name' },
+    { label: 'Email', name: 'email', type: 'email', placeholder: 'Enter your email' },
+    { label: 'Phone', name: 'phone', type: 'tel', placeholder: 'Enter your phone number' },
+    { label: 'Comments', name: 'comments', type: 'text', placeholder: 'Enter your comments or message' }
   ];
 
   const handleFormSubmit = () => {
@@ -19,9 +27,19 @@ function App() {
       <h1>Dynamic Form Test</h1>
       <div className="Page">
         <FormCreator
-          formTemplate={formTemplate}
+          formTemplate={formTemplate1}
           formName="User Registration Form"
           submitButtonLabel="Register"
+          onSubmit={handleFormSubmit}
+        />
+      </div>
+      <br />
+      <br />
+      <div className="Page">
+        <FormCreator
+          formTemplate={formTemplate2}
+          formName="Feedback"
+          submitButtonLabel="Send Feedback"
           onSubmit={handleFormSubmit}
         />
       </div>
