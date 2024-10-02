efc861034dc939c3c743e0f78ea0a084c2c26d1c
bahylol <bahymohamed2010@gmail.com>
2024-10-02 14:59:14 +0300

    added another example

--- src/App.js
+++ src/App.js
@@ -1,54 +1,106 @@
 import './App.css';
+import * as yup from 'yup';
 import FormCreator from './components/FormCreator';
 
 function App() {
 
   const formTemplate1 = [
-    { label: 'First Name', name: 'firstName', type: 'text', placeholder: 'John' },
-    { label: 'Last Name', name: 'lastName', type: 'text', placeholder: 'Doe' },
-    { label: 'Email', name: 'email', type: 'email', placeholder: 'john.doe@example.com' },
-    { label: 'Phone', name: 'phone', type: 'tel', placeholder: '+1234567890' },
-  ];
-
-  const formTemplate2 = [
-    { label: 'First Name', name: 'firstName', type: 'text', placeholder: 'John' },
-    { label: 'Last Name', name: 'lastName', type: 'text', placeholder: 'Doe' },
-    { label: 'Email', name: 'email', type: 'email', placeholder: 'john.doe@example.com' },
-    { label: 'Phone', name: 'phone', type: 'tel', placeholder: '+1234567890' },
-    { label: 'Comments', name: 'comments', type: 'text', placeholder: 'Your message here...' },
-  ];
-
-  const formTemplate3 = [
-    { label: 'First Name', name: 'firstName', type: 'text', placeholder: 'Enter your first name', required: true },
-    { label: 'Last Name', name: 'lastName', type: 'text', placeholder: 'Enter your last name', required: true },
-    { label: 'Email', name: 'email', type: 'email', placeholder: 'Enter your email', required: true },
-    { label: 'Phone', name: 'phone', type: 'tel', placeholder: 'Enter your phone number', required: false },
     {
-      label: 'Favorite Color',
-      name: 'color',
-      type: 'dropdown',
-      required: true,
-      options: [
-        { value: 'red', label: 'Red' },
-        { value: 'blue', label: 'Blue' },
-        { value: 'green', label: 'Green' },
-      ],
+      label: 'First Name',
+      name: 'firstName',
+      type: 'text',
+      placeholder: 'Enter your first name',
+      validation: yup.string().required('First Name is required')
+    },
+    {
+      label: 'Last Name',
+      name: 'lastName',
+      type: 'text',
+      placeholder: 'Enter your last name',
+      validation: yup.string().required('Last Name is required')
+    },
+    {
+      label: 'Email',
+      name: 'email',
+      type: 'email',
+      placeholder: 'Enter your email',
+      validation: yup.string().email('Must be a valid email').required('Email is required')
+    },
+    {
+      label: 'Phone',
+      name: 'phone',
+      type: 'tel',
+      placeholder: 'Enter your phone number',
+      validation: yup.string().matches(/^[0-9]+$/, 'Phone number must contain only digits')
     },
     {
       label: 'Gender',
       name: 'gender',
       type: 'radio',
-      required: true,
-      options: [
-        { value: 'male', label: 'Male' },
-        { value: 'female', label: 'Female' },
-      ],
+      options: [{ label: 'Male', value: 'male' }, { label: 'Female', value: 'female' }],
     },
+    {
+      label: 'Country',
+      name: 'country',
+      type: 'select',
+      options: ['Please Select', 'USA', 'Canada', 'Germany'],
+      validation: yup.string().required('Please select a country')
+    }
   ];
 
+
+  const formTemplate2 = [
+    {
+      label: 'First Name',
+      name: 'firstName',
+      type: 'text',
+      placeholder: 'Enter your first name',
+      validation: yup.string().required('First Name is required')
+    },
+    {
+      label: 'Last Name',
+      name: 'lastName',
+      type: 'text',
+      placeholder: 'Enter your last name',
+      validation: yup.string().required('Last Name is required')
+    },
+    {
+      label: 'Email',
+      name: 'email',
+      type: 'email',
+      placeholder: 'Enter your email',
+      validation: yup.string().email('Must be a valid email').required('Email is required')
+    },
+    {
+      label: 'Comments',
+      name: 'comments',
+      type: 'textarea',
+      placeholder: 'Enter your feedback here',
+      validation: yup.string().required('Feedback is required')
+    },
+    {
+      label: 'Rating',
+      name: 'rating',
+      type: 'radio',
+      options: [
+        { label: 'Terrible', value: '1' },
+        { label: 'Bad', value: '2' },
+        { label: 'Average', value: '3' },
+        { label: 'Good', value: '4' },
+        { label: 'Excellent', value: '5' }
+      ],
+      validation: yup.string().required('Rating is required')
+    }
+  ];
+
+
+
   const handleFormSubmit = (formData) => {
-    console.log(formData);
-    alert(`Form submitted successfully!`);
+    const formDataString = Object.entries(formData)
+      .map(([key, value]) => `${key}: ${value}`)
+      .join('\n');
+
+    alert(`Form submitted successfully! \n\n${formDataString}`);
   };
 
   return (
@@ -57,7 +109,7 @@ function App() {
 
       <div className="Page">
         <FormCreator
-          formTemplate={formTemplate3}
+          formTemplate={formTemplate1}
           formName="User Registration Form"
           submitButtonLabel="Register"
           onSubmit={handleFormSubmit}
